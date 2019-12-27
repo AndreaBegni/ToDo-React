b6e6ef3a5d34478ea87e7f82d404dd1a2851c38c
AndreaBegni <braintechno1@gmail.com>
2019-12-27 20:48:49 +0100

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function App() {
-  return <div className="App">
-      
-  </div>;
+  return (
+    <div className="App">
+      <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+    </div>
+  );
 }
 
 export default App;
