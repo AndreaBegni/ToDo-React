b079a095062d812b3a86ce72e30aad2658f266c1
AndreaBegni <braintechno1@gmail.com>
2019-12-28 02:35:18 +0100

    added ToDoList renderer component with some tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navigation from './Components/Navigation'
+import ToDoList from './Components/ToDoList'
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
+      <ToDoList></ToDoList>
     </div>
   );
 }
---
+++ src/Components/ToDoList.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+
+export default class ToDoList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [
+        { title: "Cosa da fare 1", description: "descrizione cosa da fare 1" },
+        { title: "Cosa da fare 2", description: "descrizione cosa da fare 2" },
+        { title: "Cosa da fare 3", description: "descrizione cosa da fare 3" }
+      ]
+    };
+  }
+
+  render() {
+    const { todos } = this.state;
+    const renderTodos = todos.map((todo, i) => (
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey={i}>
+          {todo.title}
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={i}>
+          <Card.Body>{todo.description}</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    ));
+
+    return (
+      <Accordion>
+        {renderTodos}
+      </Accordion>
+    );
+  }
+}
