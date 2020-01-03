041537cf990f381e800e0fa2c3ef2a3b9f4e00db
AndreaBegni <braintechno1@gmail.com>
2020-01-03 12:16:27 +0100

    made App a component used the App component state to keep track of todos and added 2 methods loadToDos to load the initial todos from the localStorage and updateToDos to update the state with new todos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import Navigation from "./Components/Navigation";
 import ToDoList from "./Components/ToDoList";
 import FormNewToDo from "./Components/FormNewToDo";
-import todoJSON from "./todos.json"
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation></Navigation>
-      <FormNewToDo></FormNewToDo>
-      <ToDoList todoJSON={todoJSON}></ToDoList>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoJSON: this.loadToDos()
+    };
+
+    this.updateToDos = this.updateToDos.bind(this);
+    this.loadToDos = this.loadToDos.bind(this);
+  }
+
+  loadToDos = () => {
+    //localStorage.setItem("todoJSON", JSON.stringify(list));
+    if (localStorage.getItem("todoJSON") != null) {
+      return JSON.parse(localStorage.getItem("todoJSON"));
+    }
+  };
+
+  updateToDos = newTodos => {
+    this.setState(
+      {
+        todoJSON: newTodos
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation></Navigation>
+        <FormNewToDo
+          todoJSON={this.state.todoJSON}
+          updateToDos={this.updateToDos}
+        ></FormNewToDo>
+        <ToDoList todoJSON={this.state.todoJSON}></ToDoList>
+      </div>
+    );
+  }
 }
-
-export default App;
